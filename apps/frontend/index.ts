ae66c1123ddc78740468bb8cd7cd60e67cc5170c
Ras <siregar.riyando@gmail.com>
2025-03-26 13:29:06 +0800

    modify(db-schema): adding basePrice

--- apps/frontend/index.ts
+++ apps/frontend/index.ts
@@ -3,23 +3,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // await prisma.user.create({
-  //   data: {
-  //     wallet: "0x123456789abcdef123456789abcdef1234567890",
-  //     email: "user@example.com",
-  //     username: "nftcreduser",
-  //     profilePic: "https://example.com/profile.jpg",
-  //     userType: "BORROWER",
-  //     credentials: {},
-  //     loans: {},
-  //   },
-  // });
 
-  // const allUsers = await prisma.user.findMany();
-  // console.log(allUsers);
-
-  const registeredNFTs = await prisma.registeredNFT.findMany();
-  console.log(registeredNFTs);
+  const data = await prisma.credentialType.findMany();
+  console.log(data);
 }
 
 main()
