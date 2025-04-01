65d94253ec4ac312d53e80b61c68863783254aed
Ras <siregar.riyando@gmail.com>
2025-04-01 13:14:12 +0800

    modify(backend): merge credential api endpoint into 1 file

--- apps/frontend/app/api/nft/credential/route.ts
+++ apps/frontend/app/api/nft/credential/route.ts
@@ -59,3 +59,32 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "userId is required" },
+        { status: 400 }
+      );
+    }
+
+    const credentials = await prisma.credential.findMany({
+      where: { userId },
+      include: {
+        credentialType: true,
+      },
+    });
+
+    return NextResponse.json(credentials, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching Credentials:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
