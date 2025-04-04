cb66a7ec1ba7b0ceb2ee54db1cb7f7dc3d71d9d9
Agung Wicaksono <agungwck99@gmail.com>
2025-04-04 18:38:34 +0700

    refactor(types): redefine Ethereum interface and enhance Window type declaration

--- apps/frontend/app/types/window.d.ts
+++ apps/frontend/app/types/window.d.ts
@@ -1,11 +1,17 @@
-interface Window {
-  ethereum?: {
-    request: (args: { method: string; params?: any[] }) => Promise<any>;
-    isMetaMask?: boolean;
-    on?: (...args: any[]) => void;
-    removeListener?: (...args: any[]) => void;
-    selectedAddress?: string | null;
-    networkVersion?: string;
-    chainId?: string;
-  };
+interface Ethereum {
+  request: (args: { method: string; params?: any[] }) => Promise<any>;
+  isMetaMask?: boolean;
+  on: (event: string, callback: (...args: any[]) => void) => void;
+  removeListener: (event: string, callback: (...args: any[]) => void) => void;
+  selectedAddress?: string;
+  networkVersion?: string;
+  chainId?: string;
 }
+
+declare global {
+  interface Window {
+    ethereum: Ethereum;
+  }
+}
+
+export {};
